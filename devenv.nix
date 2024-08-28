2e65bbaa62feb85a9f47d309a15b3fe83fad45f1
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2024-08-28 14:50:49 -0400

    For NixOS

---
+++ devenv.nix
@@ -0,0 +1,19 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.rust.enable = true;
+
+  enterShell = ''
+    fish
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+}
