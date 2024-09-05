562c74fc3f322cbf8b60314387f304facaf957b8
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2024-09-04 21:12:17 -0400

    Dealing with newlines and spaces

--- src/lexer.rs
+++ src/lexer.rs
@@ -25,73 +25,80 @@ impl<'a> Iterator for Lexer<'a> {
     type Item = Result<Token<'a>, AnalisisError>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(c) = self.input.chars().nth(self.index) {
-            let initial_index = self.index;
-            self.index += 1;
-
-            let token_type: TokenType = match c {
-                '(' => TokenType::LeftParenthesis,
-                ')' => TokenType::RightParenthesis,
-                '{' => TokenType::LeftBracket,
-                '}' => TokenType::RightBracket,
-                '*' => TokenType::Asterisk,
-                '.' => TokenType::Dot,
-                ',' => TokenType::Comma,
-                '+' => TokenType::Plus,
-                '-' => TokenType::Minus,
-                ';' => TokenType::SemiColon,
-                '!' => match self.input.chars().nth(self.index) {
-                    Some('=') => {
-                        self.index += 1;
-                        TokenType::BangEqual
-                    }
-                    _ => TokenType::Bang,
-                },
-                '=' => match self.input.chars().nth(self.index) {
-                    Some('=') => {
-                        self.index += 1;
-                        TokenType::EqualEqual
-                    }
-                    _ => TokenType::Equal,
-                },
-                '>' => match self.input.chars().nth(self.index) {
-                    Some('=') => {
-                        self.index += 1;
-                        TokenType::GreaterEqual
-                    }
-                    _ => TokenType::Greater,
-                },
-                '<' => match self.input.chars().nth(self.index) {
-                    Some('=') => {
-                        self.index += 1;
-                        TokenType::LessEqual
-                    }
-                    _ => TokenType::Less,
-                },
-                c => return Some(Err(AnalisisError::UnrecognizedCharacter(self.line, c))),
-            };
-
-            let token = Token::new(
-                token_type,
-                &self.input[initial_index..self.index],
-                Literal::Null,
-                self.line,
-            );
-
-            Some(Ok(token))
-        } else {
-            if self.index == self.input.len() {
+        loop {
+            if let Some(c) = self.input.chars().nth(self.index) {
+                let initial_index = self.index;
                 self.index += 1;
 
-                return Some(Ok(Token::new(
-                    TokenType::EndOfFile,
-                    "",
+                let token_type: TokenType = match c {
+                    '(' => TokenType::LeftParenthesis,
+                    ')' => TokenType::RightParenthesis,
+                    '{' => TokenType::LeftBracket,
+                    '}' => TokenType::RightBracket,
+                    '*' => TokenType::Asterisk,
+                    '.' => TokenType::Dot,
+                    ',' => TokenType::Comma,
+                    '+' => TokenType::Plus,
+                    '-' => TokenType::Minus,
+                    ';' => TokenType::SemiColon,
+                    '!' => match self.input.chars().nth(self.index) {
+                        Some('=') => {
+                            self.index += 1;
+                            TokenType::BangEqual
+                        }
+                        _ => TokenType::Bang,
+                    },
+                    '=' => match self.input.chars().nth(self.index) {
+                        Some('=') => {
+                            self.index += 1;
+                            TokenType::EqualEqual
+                        }
+                        _ => TokenType::Equal,
+                    },
+                    '>' => match self.input.chars().nth(self.index) {
+                        Some('=') => {
+                            self.index += 1;
+                            TokenType::GreaterEqual
+                        }
+                        _ => TokenType::Greater,
+                    },
+                    '<' => match self.input.chars().nth(self.index) {
+                        Some('=') => {
+                            self.index += 1;
+                            TokenType::LessEqual
+                        }
+                        _ => TokenType::Less,
+                    },
+                    '\n' => {
+                        self.line += 1;
+                        continue;
+                    }
+                    c if c.is_whitespace() => continue,
+                    c => return Some(Err(AnalisisError::UnrecognizedCharacter(self.line, c))),
+                };
+
+                let token = Token::new(
+                    token_type,
+                    &self.input[initial_index..self.index],
                     Literal::Null,
                     self.line,
-                )));
-            }
+                );
 
-            return None;
+                return Some(Ok(token));
+            } else {
+                if self.index == self.input.len() {
+                    self.index += 1;
+
+                    return Some(Ok(Token::new(
+                        TokenType::EndOfFile,
+                        "",
+                        Literal::Null,
+                        self.line,
+                    )));
+                }
+
+                return None;
+            }
         }
     }
 }
