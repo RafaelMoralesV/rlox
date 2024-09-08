0fea91c5b750bc7603b1fcfb8faef2a75ce2a73a
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2024-09-08 19:28:11 -0300

    Using Clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,65 @@
 mod lexer;
 mod token;
 
-use std::env;
 use std::fs;
 use std::process::ExitCode;
 
+use clap::Parser;
+use clap::Subcommand;
 use lexer::AnalisisError;
 
 use crate::lexer::Lexer;
 
-fn main() -> ExitCode {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        eprintln!("Usage: {} tokenize <filename>", args[0]);
-        return ExitCode::SUCCESS;
-    }
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    let command = &args[1];
-    let filename = &args[2];
+#[derive(Subcommand)]
+enum Commands {
+    /// Creates a list of the Tokens inside the provided Filename.
+    Tokenize { filename: String },
+
+    /// Parses the provided Filename into an AST.
+    Parse { filename: String },
+}
+
+fn main() -> ExitCode {
+    let args = Args::parse();
 
     let mut errors_found = false;
 
-    match command.as_str() {
-        "tokenize" => {
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+    match args.command {
+        Commands::Tokenize { filename } => {
+            let file_contents = fs::read_to_string(&filename).unwrap_or_else(|_| {
                 eprintln!("Failed to read file {}", filename);
                 String::new()
             });
 
             // Uncomment this block to pass the first stage
-            if !file_contents.is_empty() {
-                for token in Lexer::new(&file_contents) {
-                    match token {
-                        Ok(token) => println!("{token}"),
-                        Err(e) => {
-                            errors_found = true;
-                            match e {
-                                AnalisisError::UnrecognizedCharacter(line, c) => {
-                                    eprintln!("[line {line}] Error: Unexpected character: {c}")
-                                }
-                                AnalisisError::UnterminatedString(line) => {
-                                    eprintln!("[line {line}] Error: Unterminated string.")
-                                }
+            for token in Lexer::new(&file_contents) {
+                match token {
+                    Ok(token) => println!("{token}"),
+                    Err(e) => {
+                        errors_found = true;
+                        match e {
+                            AnalisisError::UnrecognizedCharacter(line, c) => {
+                                eprintln!("[line {line}] Error: Unexpected character: {c}")
+                            }
+                            AnalisisError::UnterminatedString(line) => {
+                                eprintln!("[line {line}] Error: Unterminated string.")
                             }
                         }
                     }
                 }
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
             }
         }
-        _ => {
-            eprintln!("Unknown command: {}", command);
-            return ExitCode::FAILURE;
+        Commands::Parse { filename: _ } => {
+            todo!();
         }
-    }
+    };
 
     if errors_found {
         ExitCode::from(65)
